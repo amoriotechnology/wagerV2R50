4a07951698470bee12be394a715f2c2cb95fbc89
SuryaAmoriotech <suryakala@amoriotech.com>
2024-11-15 16:53:57 +0530

    state_tax

--- application/views/include/admin_home.php
+++ application/views/include/admin_home.php
@@ -1,28 +1,12 @@
 
 <!-- Admin Home Start -->
 <div class="content-wrapper">
-<<<<<<< HEAD
-   <!-- Content Header (Page header) -->
-   <section class="content-header">
-      <div class="header-icon">
- 
-      
-      
-            <figure class="one">
-               <img src="<?php echo base_url()  ?>asset/images/dashboard.png"  class="headshotphoto" style="height:50px;" />
-      </div>
 
-      
-      
-      
-=======
    <section class="content-header">
       <div class="header-icon">
          <figure class="one">
          <img src="<?php echo base_url()  ?>asset/images/dashboard.png"  class="headshotphoto" style="height:50px;" />
       </div>
-
->>>>>>> 96da4c5f7f8e43cc5e30ba4b720f93e3fdc6b303
       <div class="header-title">
          <h1><?php  echo "Dashboard" ;?></h1>
          <small><?php echo display('home') ?></small>
