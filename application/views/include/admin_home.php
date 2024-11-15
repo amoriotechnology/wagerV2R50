2b627837210b6b9cd0df551c895a93e5d924643c
Yokeshdev01 <yokesh@amoriotech.com>
2024-11-15 13:11:55 +0530

    simplify

---
+++ application/views/include/admin_home.php
@@ -0,0 +1,26 @@
+
+<!-- Admin Home Start -->
+<div class="content-wrapper">
+   <!-- Content Header (Page header) -->
+   <section class="content-header">
+      <div class="header-icon">
+ 
+      
+      
+            <figure class="one">
+               <img src="<?php echo base_url()  ?>asset/images/dashboard.png"  class="headshotphoto" style="height:50px;" />
+      </div>
+
+      
+      
+      
+      <div class="header-title">
+         <h1><?php  echo "Dashboard" ;?></h1>
+         <small><?php echo display('home') ?></small>
+         <ol class="breadcrumb">
+            <li><a href="#"><i class="pe-7s-home"></i> <?php echo display('home') ?></a></li>
+            <li class="active"><?php echo display('dashboard') ?></li>
+         </ol>
+      </div>
+   </section>
+</div>
